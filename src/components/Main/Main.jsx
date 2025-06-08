61afb54b0f0441df2000ef7bf1050464f21cb1be
wooki <hyunook02@naver.com>
2025-06-08 18:36:25 +0900

    feat(#31): 메인 컴포넌트 마운트시 유저 정보 요청

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import Header from '../Header/Header'
+import reissueToken from "../../global/reissueToken";
+
 
 export default function Main() {
+  const [user, setUser] = useState(null);
+  
+  useEffect(() => {
+    if (localStorage.getItem('accessToken')) {
+      fetch(`${process.env.REACT_APP_SERVER_URL}/auth/detail`, {
+        headers: {
+          'Content-Type': 'application/json',
+          'authorization': localStorage.getItem('accessToken'),
+        },
+      })
+        .then(res => res.json())
+        .then(res => {
+          if (res.code === 4010) {
+            reissueToken(this);
+          }
+          setUser({
+            id: res.data.userId,
+            username: res.data.username,
+            univ: res.data.univ,
+          });
+        });
+    }
+  }, []);
+
   return (
     <div>
-      <Header />
-      <Outlet />
+      <Header user={user}/>
+      <Outlet user={user}/>
     </div>
   );
 }
