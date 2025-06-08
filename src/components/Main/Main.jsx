74d07e61b5034895d174aea333bbd89106759bbb
wooki <hyunook02@naver.com>
2025-06-08 19:32:09 +0900

    feat(#31): 유저 정보 얻는 함수 프롭 전달

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -8,31 +8,40 @@ export default function Main() {
   const [user, setUser] = useState(null);
   
   useEffect(() => {
-    if (localStorage.getItem('accessToken')) {
-      fetch(`${process.env.REACT_APP_SERVER_URL}/auth/detail`, {
-        headers: {
-          'Content-Type': 'application/json',
-          'authorization': localStorage.getItem('accessToken'),
-        },
-      })
-        .then(res => res.json())
-        .then(res => {
-          if (res.code === 4010) {
-            reissueToken(this);
-          }
-          setUser({
-            id: res.data.userId,
-            username: res.data.username,
-            univ: res.data.univ,
-          });
-        });
-    }
+    if (localStorage.getItem('accessToken'))
+      getUserDetail();
   }, []);
+
+  const getUserDetail = () => {
+    fetch(`${process.env.REACT_APP_SERVER_URL}/auth/detail`, {
+      headers: {
+        'Content-Type': 'application/json',
+        'authorization': localStorage.getItem('accessToken'),
+      },
+    })
+      .then(res => res.json())
+      .then(res => {
+        if (res.code === 4010) {
+          reissueToken(getUserDetail);
+        }
+        setUser({
+          id: res.data.userId,
+          username: res.data.username,
+          univ: res.data.univ,
+        });
+      });
+  }
 
   return (
     <div>
-      <Header user={user}/>
-      <Outlet user={user}/>
+      <Header
+          user={user}
+          getUserDetail={getUserDetail}
+      />
+      <Outlet
+          user={user}
+          getUserDetail={getUserDetail}
+      />
     </div>
   );
 }
