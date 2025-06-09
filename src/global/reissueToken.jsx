4ff6e08268355fe239d2a856b28d18660226b60a
wooki <hyunook02@naver.com>
2025-06-09 23:21:28 +0900

    feat(#31): 토큰 재발급 예외 처리

--- src/global/reissueToken.jsx
+++ src/global/reissueToken.jsx
@@ -6,16 +6,16 @@ export default async function reissueToken(next) {
       'authorization': localStorage.getItem('refreshToken'),
     }
   })
-    .then(res => res.json()) 
-    .then(res => {
-      if (res.status === 401) {
+      .then(res => res.json())
+      .then(res => {
+          localStorage.setItem('accessToken', res.accessToken);
+          localStorage.setItem('refreshToken', res.refreshToken);
+          next && next();
+      })
+      .catch( err => {
+        console.log(err);
         alert('로그인 세션이 만료되었습니다. 다시 로그인 해주세요.');
         localStorage.removeItem('accessToken');
         localStorage.removeItem('refreshToken');
-        return;
-      }
-      localStorage.setItem('accessToken', res.accessToken);
-      localStorage.setItem('refreshToken', res.refreshToken);
-      next();
-    })
+      })
 }
