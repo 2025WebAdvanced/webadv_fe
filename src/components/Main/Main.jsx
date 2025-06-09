668ef10a44151e914e22c4ab04112a971d0549ae
wooki <hyunook02@naver.com>
2025-06-09 21:35:39 +0900

    feat(#31): 메인화면 유저 정보 패치 작성

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,7 +3,6 @@ import { Outlet } from "react-router-dom";
 import Header from '../Header/Header'
 import reissueToken from "../../global/reissueToken";
 
-
 export default function Main() {
   const [user, setUser] = useState(null);
   
@@ -13,23 +12,28 @@ export default function Main() {
   }, []);
 
   const getUserDetail = () => {
-    fetch(`${process.env.REACT_APP_SERVER_URL}/auth/detail`, {
+    fetch(`${process.env.REACT_APP_SERVER_URL}/user/detail`, {
       headers: {
         'Content-Type': 'application/json',
         'authorization': localStorage.getItem('accessToken'),
       },
     })
-      .then(res => res.json())
-      .then(res => {
-        if (res.code === 4010) {
-          reissueToken(getUserDetail);
-        }
-        setUser({
-          id: res.data.userId,
-          username: res.data.username,
-          univ: res.data.univ,
-        });
-      });
+        .then(res => {
+          if (!res.ok)
+            throw new Error(res.statusText);
+          return res.json();
+        })
+        .then(res => {
+          setUser({
+            id: res.data.id,
+            username: res.data.username,
+            univ: res.data.univ,
+          });
+        })
+        .catch(err => {
+          console.log(err);
+          reissueToken();
+        })
   }
 
   return (
