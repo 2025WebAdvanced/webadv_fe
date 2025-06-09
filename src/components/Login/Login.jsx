77be10f8c8b19331b28ac1f9a0272c92e14a3632
wooki <hyunook02@naver.com>
2025-06-10 04:36:16 +0900

    feat(#41): 로그인 시 메인 화면으로 이동

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -28,7 +28,7 @@ export default function Login() {
         .then(res => {
             localStorage.setItem('accessToken', res.accessToken);
             localStorage.setItem('refreshToken', res.refreshToken);
-            navigate(-1);
+            navigate('/board');
         })
         .catch((err) => {
           const errorMsg = '로그인에 실패했습니다. 이메일과 비밀번호를 확인해주세요.';
